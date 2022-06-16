16e42057353a07821ed0232a003e2eb9b5ca393d
Grégory Ployart <gregory.ployartwork@gmail.com>
2022-06-16 14:46:16 +0200

    first homeview componenent done

---
+++ VeatUAM/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,13 @@
+using System.Windows.Media;
+using VeatUAM.Core;
+
+namespace VeatUAM.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+            
+        }
+    }
+}
