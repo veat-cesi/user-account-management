3104c9c4fafd769aa10958c9a1d687c9df0d7ac7
Grégory Ployart <gregory.ployartwork@gmail.com>
2022-06-21 15:24:34 +0200

    Delivery Model added

---
+++ VeatUAM/MVVM/Model/DeliveryModel.cs
@@ -0,0 +1,56 @@
+using System.ComponentModel;
+
+namespace VeatUAM.MVVM.Model
+{
+    public class DeliveryModel : INotifyPropertyChanged
+    {
+        private int _id;
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+        private string _phone;
+        private string _password;
+
+        public int Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        public string FirstName
+        {
+            get => _firstName;
+            set => _firstName = value;
+        }
+
+        public string LastName
+        {
+            get => _lastName;
+            set => _lastName = value;
+        }
+
+        public string Email
+        {
+            get => _email;
+            set => _email = value;
+        }
+
+        public string Phone
+        {
+            get => _phone;
+            set => _phone = value;
+        }
+
+        public string Password
+        {
+            get => _password;
+            set => _password = value;
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;  
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
