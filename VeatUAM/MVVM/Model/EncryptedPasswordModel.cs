3c4418035b3d3a22fa0143ca9dbf17a83e5283f9
Grégory Ployart <gregory.ployartwork@gmail.com>
2022-06-27 14:28:19 +0200

    User model and EcryptedPassword model added

---
+++ VeatUAM/MVVM/Model/EncryptedPasswordModel.cs
@@ -0,0 +1,16 @@
+﻿namespace LoginTuto.Services
+{
+    public class EncryptedPasswordModel
+    {
+        public EncryptedPasswordModel(string rawPassword)
+        {
+            RawPassword = rawPassword;
+            HashedPassword = BCrypt.Net.BCrypt.HashPassword(RawPassword);
+        }
+
+        public string RawPassword { get; set; }
+
+        public string HashedPassword { get; set; }
+        
+    }
+}
