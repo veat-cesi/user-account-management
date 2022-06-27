b7cee208a15f464993d196d4ccdda03027291d81
Grégory Ployart <gregory.ployartwork@gmail.com>
2022-06-27 14:29:55 +0200

    Password service added

---
+++ VeatUAM/_Services/PasswordEncoderService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LoginTuto.Services
+{
+    public static class PasswordEncoderService
+    {
+
+        public static EncryptedPasswordModel EncryptedPassword(string password)
+        {
+            return new EncryptedPasswordModel(password);
+        }
+
+        public static string Hash(string password)
+        {
+            var encryptedPasswordModel = EncryptedPassword(password);
+            if (VerifyPassword(password, encryptedPasswordModel.HashedPassword))
+            {
+                return encryptedPasswordModel.HashedPassword;
+            }
+
+            throw new Exception("Hash processus failed.");
+        }
+
+        public static bool VerifyPassword(string input, string reference)
+        {
+            return BCrypt.Net.BCrypt.Verify(input, reference);
+        }
+    }
+}
