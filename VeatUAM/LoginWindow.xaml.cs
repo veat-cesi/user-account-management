c8cf379cf7ae153d8e14a9dd2f255582157636bc
Grégory Ployart <gregory.ployartwork@gmail.com>
2022-06-27 16:57:18 +0200

    Auth system done

--- VeatUAM/LoginWindow.xaml.cs
+++ VeatUAM/LoginWindow.xaml.cs
@@ -1,5 +1,9 @@
+using System;
+using System.Security.Authentication.ExtendedProtection;
 using System.Windows;
 using System.Windows.Input;
+using LoginTuto.Services;
+using VeatUAM._Services;
 using VeatUAM.Core;
 
 namespace VeatUAM
@@ -10,8 +14,11 @@ namespace VeatUAM
         {
             WpfSingleInstance.Make("Veat UAM - Login", uniquePerUser: false);
             InitializeComponent();
+            UserToken = new AuthenticationService();
         }
-        
+
+        public static AuthenticationService UserToken { get; set; }
+
         private void DragWindow(object sender, MouseButtonEventArgs mouseButtonEventArgs)
         {
             if (mouseButtonEventArgs.LeftButton == MouseButtonState.Pressed)
@@ -27,14 +34,46 @@ namespace VeatUAM
 
         private void Login(object sender, RoutedEventArgs routedEventArgs)
         {
-            if (true)
+            try
             {
-                WindowState = WindowState.Maximized;
-                var mainWindow = new MainWindow();
-                //Close();
-                mainWindow.ShowDialog();
+                var mySqlConnection = new MySqlConnection();
+                mySqlConnection.Connection.Open();
+                if (!mySqlConnection.ConnectionState()) return;
+                var query = $"SELECT email, password, role, firstName FROM tech WHERE email = '{LoginEmail.Text}';";
+                mySqlConnection.SetupQuery(query);
+                mySqlConnection.SetupReader();
+                while (mySqlConnection.Reader.Read())
+                {
+                    if (!PasswordEncoderService.VerifyPassword(LoginPassword.Password, mySqlConnection.Reader.GetString(1))) continue;
+                    UserToken.Email = LoginEmail.Text;
+                    UserToken.Role = mySqlConnection.Reader.GetString(2);
+                    UserToken.FirstName = mySqlConnection.Reader.GetString(3);
+                    UserToken.Connected = true;
+                    var mainWindow = new MainWindow();
+                    Close();
+                    mainWindow.ShowDialog();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("No user found with this email/password", e);
             }
         }
-        
+
+        private void LoginEmail_OnGotFocus(object sender, RoutedEventArgs e)
+        {
+            if (LoginEmail.Text == "Email")
+            {
+                LoginEmail.Text = "";
+            }
+        }
+
+        private void LoginPassword_OnGotFocus(object sender, RoutedEventArgs e)
+        {
+            if (LoginPassword.Password == "Password")
+            {
+                LoginPassword.Password = "";
+            }
+        }
     }
 }
---
+++ VeatUAM/_Services/AuthenticationService.cs
@@ -0,0 +1,13 @@
+namespace VeatUAM._Services
+{
+    public  class AuthenticationService
+    {
+        public string Email { get; set; }
+
+        public string Role { get; set; }
+
+        public string FirstName { get; set; }
+
+        public bool Connected { get; set; }
+    }
+}
