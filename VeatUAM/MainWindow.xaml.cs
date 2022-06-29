6771b4a73ff1cdee46582fc26faaacc36d61265c
Grégory Ployart <gregory.ployartwork@gmail.com>
2022-06-29 16:55:07 +0200

    Logout added

--- VeatUAM/MainWindow.xaml.cs
+++ VeatUAM/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media.Animation;
+using MahApps.Metro.IconPacks;
 using VeatUAM._Services;
 using VeatUAM.Core;
 
@@ -15,6 +17,11 @@ namespace VeatUAM
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OnClickLogout(object sender, RoutedEventArgs routedEventArgs)
+        {
+            Close();
         }
         
         private void DragWindow(object sender, MouseButtonEventArgs mouseButtonEventArgs)
@@ -31,12 +38,27 @@ namespace VeatUAM
         }
         private void MaximizeWindow(object sender, RoutedEventArgs routedEventArgs)
         {
-            WindowState = WindowState switch
+            switch (WindowState)
             {
-                WindowState.Maximized => WindowState.Normal,
-                WindowState.Normal => WindowState.Maximized,
-                _ => WindowState
-            };
+                case WindowState.Maximized:
+                    WindowState = WindowState.Normal;
+                    MaximizeWindowButton.Content = new PackIconMaterial()
+                    {
+                        Kind = PackIconMaterialKind.WindowMaximize
+                    };
+                    break;
+                case WindowState.Normal:
+                    WindowState = WindowState.Maximized;
+                    MaximizeWindowButton.Content = new PackIconMaterial()
+                    {
+                        Kind = PackIconMaterialKind.WindowRestore
+                    };
+                    break;
+                case WindowState.Minimized:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
         private void MinimizeWindow(object sender, RoutedEventArgs routedEventArgs)
         {
